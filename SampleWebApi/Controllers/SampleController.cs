5fbca2074124a69b1b905745ed71fc0399a54555
Andrey Samarin <asamarin.rootkit@gmail.com>
2024-04-18 20:21:51 +0300

    Update

--- SampleWebApi/Controllers/SampleController.cs
+++ SampleWebApi/Controllers/SampleController.cs
@@ -11,7 +11,7 @@ public class SampleController(ILogger<SampleController> logger) : ControllerBase
     public async Task<string> Sample1()
     {
         logger.LogWarning($"Starting execute in {Thread.CurrentThread.ManagedThreadId}");
-        var city = await GenerateCity().ConfigureAwait(false);
+        var city = await GenerateCityAsync().ConfigureAwait(false);
         logger.LogWarning($"CallBack in {Thread.CurrentThread.ManagedThreadId}");
         return city;
     }
@@ -20,7 +20,7 @@ public class SampleController(ILogger<SampleController> logger) : ControllerBase
     public async Task<string> Sample2()
     {
         logger.LogWarning($"Starting execute in {Thread.CurrentThread.ManagedThreadId}");
-        var city = await GenerateCity();
+        var city = await GenerateCityAsync();
         logger.LogWarning($"CallBack in {Thread.CurrentThread.ManagedThreadId}");
         return city;
     }
@@ -29,7 +29,7 @@ public class SampleController(ILogger<SampleController> logger) : ControllerBase
     public Task<string> Sample3()
     {
         logger.LogWarning($"Starting execute in {Thread.CurrentThread.ManagedThreadId}");
-        var city = GenerateCity();
+        var city = GenerateCityAsync();
         logger.LogWarning($"CallBack in {Thread.CurrentThread.ManagedThreadId}");
         return city;
     }
@@ -38,23 +38,33 @@ public class SampleController(ILogger<SampleController> logger) : ControllerBase
     public async ValueTask<string> Sample4()
     {
         logger.LogWarning($"Starting execute in {Thread.CurrentThread.ManagedThreadId}");
-        var city = await GenerateCity();
+        var city = await GenerateCityAsync();
+        logger.LogWarning($"CallBack in {Thread.CurrentThread.ManagedThreadId}");
+        return city;
+    }
+    
+    [HttpGet]
+    public Task<string> Sample5()
+    {
+        logger.LogWarning($"Starting execute in {Thread.CurrentThread.ManagedThreadId}");
+        var city = GenerateCity();
         logger.LogWarning($"CallBack in {Thread.CurrentThread.ManagedThreadId}");
         return city;
     }
     
     [NonAction]
-    private async Task<string> GenerateCity()
+    private async Task<string> GenerateCityAsync()
     {
-        var task = new List<int>(10000);
-        for (var i = 0; i < 10000; i++)
-        {
-            task.Add(i);
-        }
-        var httClient = new HttpClient();
-        var response = await httClient.GetAsync("https://google.com");
+        var response = await new HttpClient().GetStringAsync("https://ya.ru");
         logger.LogWarning($"IO in {Thread.CurrentThread.ManagedThreadId}");
-        await response.Content.ReadAsStringAsync();
-        return "Hello Google";
+        return response;
+    }
+    
+    [NonAction]
+    private Task<string> GenerateCity()
+    {
+        var response = new HttpClient().GetStringAsync("https://ya.ru");
+        logger.LogWarning($"IO in {Thread.CurrentThread.ManagedThreadId}");
+        return response;
     }
 }
